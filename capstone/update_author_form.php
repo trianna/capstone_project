d27fa3bbdb47b8c5424e295338c25aeaa571e8d5
trianna <31740007+trianna@users.noreply.github.com>
2018-10-31 16:30:59 -0500

    Add files via upload

--- capstone/update_author_form.php
+++ capstone/update_author_form.php
@@ -1,41 +1,27 @@
 <?php
-require_once ('includes/check_session.inc.php'); // check session
-$page_title = 'Update a Author';
-include ('includes/header.inc.html');
-require_once ('../../mysqli_connect_capstone.php');
-echo "<div class=\"page-header\"><h3>Update Authors</h3></div>";
+require_once('includes/check_session.inc.php'); //check session
+$page_title = 'Update an Author';
+include('includes/header.inc.html');
+require_once('../../mysqli_connect_capstone.php');
+echo"<div class=\"page-header\"><h3>Update Authors</h3></div>";
 $id = $_GET['id'];
 $query = "SELECT * FROM author WHERE author_id=$id";
-$result = @mysqli_query($dbc, $query);
+$result = @mysqli_query ($dbc, $query);
 $num = mysqli_num_rows($result);
-if ($num > 0) {
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+if ($num > 0)
+{
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+    {
         ?>
-<form action="update_author.php" method="post">
-
-	First Name: <input name="first_name" size=50
-		value="<?php echo $row['first_name']; ?>"><br /> <input
-		type="submit" value="update"> <input type="reset" value="reset"> <input
-		type="hidden" name="id" value="<?php echo $row['author_id'];?>">
-		
-	Middle Initial: <input name=middle_initial size=50
-		value="<?php echo $row['middle_initial']; ?>"><br /> <input
-		type="submit" value="update"> <input type="reset" value="reset"> <input
-		type="hidden" name="id" value="<?php echo $row['author_id'];?>">
-		
-	Last Name: <input name="last_name" size=50
-		value="<?php echo $row['last_name']; ?>"><br /> <input
-		type="submit" value="update"> <input type="reset" value="reset"> <input
-		type="hidden" name="id" value="<?php echo $row['author_id'];?>">
-		
-	Address: <input name="address" size=50
-		value="<?php echo $row['address']; ?>"><br /> <input
-		type="submit" value="update"> <input type="reset" value="reset"> <input
-		type="hidden" name="id" value="<?php echo $row['author_id'];?>">
-</form>
+        <form action="update_author.php" method="post">
+            Author: <input name="term" size=50 value="<?php echo $row['author_term']; ?>"><br/>
+            <input type="submit" value="update">
+            <input type="reset" value="reset">
+            <input type="hidden" name="id" value="<?php echo $row['author_id'];?>">
+        </form>
 <?php
     }
 }
 mysqli_close($dbc);
-include ('includes/footer.inc.html');
+include('includes/footer.inc.html');
 ?>
--- capstone/view_authors.php
+++ capstone/view_authors.php
@@ -1,52 +1,37 @@
 <?php
-require_once ('includes/check_session.inc.php'); // check session
-$page_title = 'View Authors';
-include_once ('../../mysqli_connect_final.php');
-echo "<div class=\"page-header\"><h3>View Authors</h3></div>";
-$id = $_GET['id'];
-
-
+require_once('includes/check_session.inc.php'); //check session
+$page_title = 'Welcome to my Site!';
+include('includes/header.inc.html');
+require_once('../../mysqli_connect_capstone.php');
+?>
+<div class="page-header"><h3>Authors</h3></div>
+<?php
 echo ("<div style=\"text-align:center\">");
-echo ("<a href=\"add.php\">Add a Document</a><p>");
+echo ("<a href=\"add_author.php\">Add a record</a><p>");
 echo ("<a href=\"searchform.php\">Search records</a><p>");
 echo ("</div>");
-$query = "SELECT * FROM article
-			JOIN article_author ON article.article_id = article_author.article_id
-			JOIN author ON article_author.author_id = author.author_id 
-			WHERE article_id=$id";
+$query = "SELECT * FROM author";
 $result = @mysqli_query($dbc, $query);
 $num = mysqli_num_rows($result);
 
-$first_row = mysql_fetch_assoc($result);
-echo "<div class=\"page-header\"><h3>View authors from ". $first_row['title'] ."</h3></div>";
-mysql_data_seek($result, 0);
-
-if ($num > 0) {
-	echo "<p><b>There are currently $num entries.</b></p>";
-	echo "<table cellpadding=\"5\" cellspacing=\"5\" border=\"1\"><tr>
-			<th scope=\"col\">FIRST</th>
-			<th scope=\"col\">M.I.</th>
-			<th scope=\"col\">LAST</th>
-			<th scope=\"col\">ADDRESS</th>
-			<th><center>*</center></th>
-			<th><center>*</center></th></tr>";
-	while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-		echo "<tr scope=\"row\"><td>" . $row['article.title'] . "</td>";
-		echo "<td>" . $row['first_name'] . "</td>";
-		echo "<td>" . $row['middle_initial'] . "</td>";
-		echo "<td>" . $row['last_name'] . "</td>";
-		echo "<td>" . $row['address'] . "</td>";
-		
-		echo "<td><a href=\"deleteconfirm_author.php?id=" . $row['author_id'] . "\">Delete</a></td>";
-		echo "<td><a href=\"update_author_form.php?id=" . $row['author_id'] . "\">Update</a></td></tr>";
-	}
-	echo "</table>";
-	mysqli_free_result($result); // frees memory
-} else {
-	echo '<p>There are currently no records.</p>';
+if ($num > 0)
+{
+    echo "<p><b>There are currently $num authors.</b></p>";
+    echo"<table cellpadding=\"5\" cellspacing=\"5\" border=\"1\"> <tr><th scope=\"col\">#</th><td scope=\"col\">AUTHOR</td></th><th><center>*</center></th><th><center>*</center></th></tr>";
+    while ($row = mysqli_fetch_array($result, MYSQL_ASSOC))
+    {
+        echo "<tr scope=\"row\"><td>".$row['author_id']."</td>";
+        echo "<td>".$row['author_term']."</td>";
+        echo "<td><a href=\"deleteconfirm_author.php?id=".$row['author_id']."\">Delete</a></td>";
+        echo "<td><a href=\"update_author_form.php?id=".$row['author_id']."\">Update</a></td></tr>";
+    }
+    echo "</table>";
+    mysqli_free_result ($result); //frees memory
 }
-
-
+else
+{
+    echo '<p>There are currently no authors.</p>';
+}
 mysqli_close($dbc);
-include ('includes/footer.inc.html');
+include('includes/footer.inc.html');
 ?>
